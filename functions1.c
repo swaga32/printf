5dbeffcd8d955dee51dff84a700992495126ced1
swaga32 <jacksonmbirui@gmail.com>
2022-08-08 10:45:43 -0700

    print correctio98

--- functions1.c
+++ functions1.c
@@ -50,7 +50,7 @@ int print_octal(va_list types, char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
-	
+
 	UNUSED(width);
 
 	num = convert_size_unsgnd(num, size);
